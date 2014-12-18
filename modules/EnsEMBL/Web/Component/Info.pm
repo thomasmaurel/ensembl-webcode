baecfade335f40b0479fa6f5e18dc0ab01d0c261
Anne Lyle <annelyle@ebi.ac.uk>
2014-12-18 14:25:56 +0000

    Add form under each image, and move caption to above the image so it doesn't look like part of the form

--- modules/EnsEMBL/Web/Component/Info.pm
+++ modules/EnsEMBL/Web/Component/Info.pm
@@ -22,8 +22,14 @@ use strict;
 
 use base qw(EnsEMBL::Web::Component);
 
+our $data_type = {
+                  'Gene'      => {'param' => 'g', 'label' => 'Choose a Gene'},
+                  'Variation' => {'param' => 'v', 'label' => 'Choose a Variant'},
+                  'Location'  => {'param' => 'r', 'label' => 'Choose Coordinates'},
+                  };
+
 sub format_gallery {
-  my ($self, @previews) = @_;
+  my ($self, $type, @previews) = @_;
   my ($html, @toc);
 
   foreach my $group (@previews) {
@@ -39,7 +45,23 @@ sub format_gallery {
     foreach my $page (@pages) {
       $html .= '<div class="gallery_preview">';
 
-      $html .= sprintf('<a href="%s"><img src="/i/gallery/%s.png" /></a><br /><div class="preview_caption"><a href="%s" class="nodeco">%s</a></div>', $page->{'url'}, $page->{'img'}, $page->{'url'}, $page->{'caption'});
+      $html .= sprintf('<div class="preview_caption"><a href="%s" class="nodeco">%s</a></div><br />', $page->{'url'}, $page->{'caption'});
+
+      $html .= sprintf('<a href="%s"><img src="/i/gallery/%s.png" /></a>', $page->{'url'}, $page->{'img'});
+
+      my $form = $self->new_form({'action' => $page->{'url'}, 'method' => 'post'});
+
+      my $data_param = $data_type->{$type}{'param'};
+      my $field = $form->add_field({
+                      'type'  => 'String',
+                      'size'  => 10,
+                      'name'  => $data_param,
+                      'label' => $data_type->{$type}{'label'},
+                      'value' => $self->hub->param($data_param),
+                      });
+      $field->add_element({'type' => 'submit', 'value' => 'Go'}, 1);
+
+      $html .= '<div style="width:300px">'.$form->render.'</div>';
 
       $html .= '</div>';
     }
