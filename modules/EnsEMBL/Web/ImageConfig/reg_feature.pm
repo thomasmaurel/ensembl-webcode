873650a852db7e50a35b672eec05d0f177a9ac8b
Anne Lyle <annelyle@ebi.ac.uk>
2015-03-19 14:44:49 +0000

    Configuration for summary image on Motif Feature page

---
+++ modules/EnsEMBL/Web/ImageConfig/reg_feature.pm
@@ -0,0 +1,43 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::ImageConfig::reg_feature;
+
+use strict;
+
+use base qw(EnsEMBL::Web::ImageConfig);
+
+sub init {
+  my $self = shift;
+  
+  $self->set_parameters({
+    sortable_tracks => 0,
+    opt_lines       => 1,
+  });  
+
+  $self->add_tracks('other',
+    [ 'scalebar', '', 'scalebar', { display => 'normal', strand => 'b', name => 'Scale bar', description => 'Shows the scalebar' }],
+    [ 'ruler',    '', 'ruler',    { display => 'normal', strand => 'b', name => 'Ruler',     description => 'Shows the length of the region being displayed' }]
+  );
+  
+  $self->load_tracks;
+  $self->image_resize = 1;
+  
+}
+
+1;
